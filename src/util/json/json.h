bba2b30cbb78675fcc810e2307de75c3babbc6b1
Riyyi <riyyi3@gmail.com>
2022-07-22 12:42:33 +0200

    Util: Add helper Util::Json to Util::JSON::Value

---
+++ src/util/json/json.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2022 Riyyi
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef UTIL_JSON_JSON_H
+#define UTIL_JSON_JSON_H
+
+#include "util/json/value.h"
+
+namespace Util {
+
+using Json = Util::JSON::Value;
+
+} // namespace Util
+
+#endif // UTIL_JSON_JSON_H
